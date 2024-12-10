02f389c4f2b56cb49439234c026c46b2b9928a97
KevinJHP19 <huamanpintokevin@fpllefia.com>
2024-12-10 18:06:19 +0100

    volviendo a añadir

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    
+ root: path.resolve(__dirname, 'src'),
+ publicDir: path.resolve(__dirname, 'public'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    build:{
+        outDir: 'dist'
+    },
+});
